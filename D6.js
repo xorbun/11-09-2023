5dcf021eb75dd61aeef5624c940bf280b4209f1e
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-09-11 22:01:55 +0200

    some bug fixes

--- D6.js
+++ D6.js
@@ -27,7 +27,7 @@ console.log(random);
 /* ESERCIZIO 3 (filter)
   Scrivi una funzione per ricavare solamente i valori PARI da un array composto da soli valori numerici
 */
- const number=[1,2,3,4,5,6,7,8,9,10];
+ const number=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
  const pari=number.filter((pari)=> pari%2===0);
  console.log(pari);
 
